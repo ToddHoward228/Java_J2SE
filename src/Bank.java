cf8ea196c032bbd1cf44242e5d88680329bb7af8
Тимофій <@DESKTOP-ULFLDS9>
2025-04-24 13:52:59 +0300

    Part 1 Synchronized.

--- src/Bank.java
+++ src/Bank.java
@@ -2,7 +2,7 @@ public class Bank {
     private int account;
     private final int END = 20000;
 
-    public void inc(){
+    public synchronized void inc(){
         account = 0;
         for (int i = 0; i < END; i++) {
             System.out.println(account);
@@ -10,7 +10,7 @@ public class Bank {
         }
         System.out.println(account);
     }
-    public void dec(){
+    public synchronized void dec(){
         account = END;
         for (int i = 0; i < END; i++) {
             System.out.println(account);
